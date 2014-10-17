18aee69abec82d73f7c28f97249731eed59e4245
Tiago Silveira <dukejeffrie@gmail.com>
2014-10-17 18:43:09 -0300

    Bitset implementation
    This is mostly a warm-up. A bitset implementation that is fast in memory
    and can be used as backing data for an input or a column.

---
+++ bitset.go
@@ -0,0 +1,95 @@
+// Bitset implementation for HTM.
+//
+// Bits are stored into a variable-length slice of 64-bit integers.
+
+package htm
+
+import "fmt"
+import "strings"
+
+// A bitset type.
+type Bitset struct {
+	// The binary representation of this bitset. Bits are stored LittleEndian but integers grow in orders of magnitude into 64-bit integers instead of bytes.
+	binary []uint64
+	// The valid length of the binary array. Whatever additional space is wasted.
+	length int
+}
+
+func (b Bitset) locate(index int) (pos, offset int) {
+	pos = index / 64
+	offset = index % 64
+	return
+}
+
+func (b Bitset) IsSet(index int) bool {
+	pos, offset := b.locate(index)
+	return b.binary[pos]&(1<<uint64(offset)) != 0
+}
+
+func (b Bitset) ToIndexes(indices []int) []int {
+	dest := 0
+	for pos, el := range b.binary {
+		for i := uint64(0); i < 64; i++ {
+			if el&(1<<i) != 0 {
+				indices[dest] = pos*64 + int(i)
+				fmt.Printf("indices[%d] = %d\n", dest, indices[dest])
+				dest++
+			}
+		}
+	}
+	return indices[0:dest]
+}
+
+func (b Bitset) NumSetBits() int {
+	// This algorithm behaves well only when the number of set bits is small.
+	count := 0
+	for _, el := range b.binary {
+		for ; el != 0; count++ {
+			el &= el - 1
+		}
+	}
+	return count
+}
+
+func (b *Bitset) Set(indices []int) {
+	idx := 0
+	for pos, el := range b.binary {
+		if indices[idx] > b.Len() {
+			panic(fmt.Sprintf(
+				"index %v is larger than the length of this bitset (length=%v",
+				indices[idx], b.Len()))
+		}
+		el = 0
+		min_idx, max_idx := pos*64, (pos+1)*64
+		for ; idx < len(indices) &&
+			indices[idx] >= min_idx &&
+			indices[idx] < max_idx; idx++ {
+			el |= 1 << uint64(indices[idx]-min_idx)
+		}
+		b.binary[pos] = el
+	}
+}
+
+func (b Bitset) String() string {
+	s := make([]string, len(b.binary))
+	for i, v := range b.binary {
+		s[i] = fmt.Sprintf("%b", v)
+	}
+	return strings.Join(s, " ")
+}
+
+func (b Bitset) Len() int {
+	return b.length
+}
+
+// Creates a new bitset with enough storage for at least the given number of bits.
+func NewBitset(length int) *Bitset {
+	result := new(Bitset)
+	num := length / 64
+	if (length % 64) > 0 {
+		num++
+	}
+	result.binary = make([]uint64, num)
+	result.length = length
+	return result
+}
---
+++ bitset_test.go
@@ -0,0 +1,76 @@
+// Unit tests for sensor implementation
+
+package htm
+
+import "fmt"
+import "testing"
+
+func ExpectEquals(t *testing.T, message string, expected interface{}, actual interface{}) {
+	if expected != actual {
+		t.Errorf("%s: expected %v, but was: %v", message, expected, actual)
+	}
+}
+
+func ExpectContentEquals(t *testing.T, message string, expected []int, actual []int) {
+	if len(expected) != len(actual) {
+		t.Errorf("%s: length differs (%v != %v), actual: %v",
+			message, len(expected), len(actual), actual)
+	}
+	failed := false
+	for i, v := range expected {
+		if v != actual[i] {
+			t.Logf("%s (element[%d]): %v != %v", message, v, actual[i])
+			failed = true
+		}
+	}
+	if failed {
+		t.Fail()
+	}
+}
+
+func TestConstruction(t *testing.T) {
+	one := NewBitset(2)
+	ExpectEquals(t, "one.length", 2, one.length)
+	ExpectEquals(t, "one.binary.length", 1, len(one.binary))
+
+	cent := NewBitset(100)
+	ExpectEquals(t, "cent.length", 100, cent.length)
+	ExpectEquals(t, "cent.binary.length", 2, len(cent.binary))
+
+	two := NewBitset(128)
+	ExpectEquals(t, "two.binary.length", 2, len(two.binary))
+
+	three := NewBitset(129)
+	ExpectEquals(t, "three.binary.length", 3, len(three.binary))
+}
+
+func TestLocate(t *testing.T) {
+	b := NewBitset(128)
+
+	for i := 0; i < 64; i++ {
+		pos, off := b.locate(i)
+		ExpectEquals(t, fmt.Sprintf("pos[%d]", i), 0, pos)
+		ExpectEquals(t, fmt.Sprintf("off[%d]", i), i, off)
+	}
+	for i := 64; i < 128; i++ {
+		pos, off := b.locate(i)
+		ExpectEquals(t, fmt.Sprintf("pos[%d]", i), 1, pos)
+		ExpectEquals(t, fmt.Sprintf("off[%d]", i), i-64, off)
+	}
+}
+
+func TestIndexing(t *testing.T) {
+	b := NewBitset(64)
+	b.Set([]int{1, 3})
+
+	ExpectEquals(t, "bits 1 and 3", "1010", b.String())
+	ExpectEquals(t, "first integer", uint64(8+2), b.binary[0])
+
+	ExpectEquals(t, "bit 1", true, b.IsSet(1))
+	ExpectEquals(t, "bit 0", false, b.IsSet(0))
+	ExpectEquals(t, "bit 3", true, b.IsSet(3))
+	ExpectEquals(t, "two bits set", 2, b.NumSetBits())
+
+	sl := make([]int, 10)
+	ExpectContentEquals(t, "indices 1 and 3", []int{1, 3}, b.ToIndexes(sl))
+}
